ac29913e45f1ee9dc91b0ab25a45de662c78ea58
Siddhant Wankhade <siddhantwankhade069@gmail.com>
2026-02-05 20:47:49 +0530

    v7.5 : Google Auth Fixed

--- backend/config/passport.config.js
+++ backend/config/passport.config.js
@@ -5,7 +5,7 @@ import User from '../models/user.model.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://backedn-host.onrender.com/api/auth/google/callback",
   },
   async(accessToken, refreshToken, profile, cb)=> {
     try {
